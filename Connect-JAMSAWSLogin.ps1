831cee73d63a5d12244ff9abfac8f9004f5c7efd
Devon Lawler <DevonL@mvpsi.com>
2014-03-06 12:06:52 -0500

    Amazon AWS Module
    Added the Connect-JAMSAWSLogin and Start-JAMSEC2 cmdlets for the Amazon
    AWS Module

---
+++ Connect-JAMSAWSLogin.ps1
@@ -0,0 +1,37 @@
+﻿<#
+.Synopsis
+   Connect to your instance of Amazon AWS
+.DESCRIPTION
+   Connect and store an Amazon AWS Session into a PowerShell variable utilizing a stored JAMS User
+.SYNTAX
+   Connect-JAMSAWSLogin -JAMSCred AmazonAWSLogin -SessionName AmazonAWS
+.EXAMPLE
+   $AmazonAWS = Connect-JAMSAWSLogin -JAMSCred AmazonAWSLogin -SessionName AmazonAWS
+#>
+Function Connect-JAMSAWSLogin($JAMSCred, $SessionName) {
+     if ($JAMSCred -eq $null) {
+        Write-Error "-JAMSCred is a required value"
+    }
+    if ($SessionName -eq $null) {
+        Write-Error "-SessionName is a required value"
+    }
+    else {
+        #
+        # Amazon EC2 Stored Credentials
+        #
+        $secretKey = (Get-JAMSCredential $JAMSCred).GetCredential($null,$null)
+        $accessKey = (Get-JAMSCredential $JAMSCred).UserName
+
+        $creds = New-AWSCredentials -AccessKey $accessKey -SecretKey $secretKey.password
+
+        #
+        # Cache our credentials for use by AWS.NET API
+        #
+        Set-AWSCredentials -Credentials $creds -StoreAs $SessionName
+
+        $storedCred = $SessionName
+        
+        return $creds
+
+    }
+}
