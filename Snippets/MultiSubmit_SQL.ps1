5d820e852fee3dfbf60128dfd5f07fb07aee65dd
GennaroP <GennaroP@mvpsi.com>
2017-03-31 10:58:23 -0400

    MultiSubmit_SQL.ps1
    A Powershell snippet to submit a job for each result of a SQL query.
    This snippet requires the SQLPS module which can be installed separately
    or with any SQL 2012 package installation as a part of SQL PS Tools.

---
+++ Snippets/MultiSubmit_SQL.ps1
@@ -0,0 +1,53 @@
+﻿Import-Module JAMS
+Import-Module SQLPS
+
+#
+#  Execute a SQL query to get a list of open orders
+#
+$query = @"
+SELECT o.order_id,
+       o.customer_id
+  FROM dbo.Orders o
+  where
+      o.order_status = 'Open'
+"@
+$openOrders = Invoke-SqlCmd localhost\SQLExpress -database SampleDB -Sql $query
+
+#
+#  Initialize an array to hold all of the jobs that we are about to submit
+#
+$processEntries = @()
+
+#
+#  Submit a job for each open order
+#
+foreach($order in $openOrders)
+{
+    #
+    #  The job that we submit (ProcessOrder) has a parameter named "id" 
+    #  and a parameter named customer_id.  We set the varaiables and
+    #  submit with the -UseVariables parameter to make that match
+    #
+    $id = $order.order_id
+    $customer_id = $order.customer_id
+    $submitResult = Submit-JAMSEntry ProcessOrder -UseVariables
+
+		#
+    #  Add the JAMSEntry number to the array
+    #
+    $processEntries += $submitResult.JAMSEntry
+}
+
+#
+#  Wait for all of the jobs we submitted to complete
+#
+Wait-JAMSEntry $processEntries -verbose
+
+#
+#  Now, check the status of each entry
+#
+foreach($entry in $processEntries)
+{
+    $entryInfo = Get-JAMSEntry -Entry $entry
+    write-host "Final status of entry " $entryInfo.JAMSEntry "is" $entryInfo.FinalStatus
+}
