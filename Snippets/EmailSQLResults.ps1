9298e12341813da4a0f9069eaea107a26fc54273
Dan Fowle-HelpSystems <86322214+DanFowle-HS@users.noreply.github.com>
2022-06-01 11:32:14 -0400

    Delete Snippets directory
    Cleaning up the V6 code from this branch, in preparation for a v7 upload

--- Snippets/EmailSQLResults.ps1
+++
@@ -1,6 +0,0 @@
-
-
-Import-Module SQLPS
-$MyQuery = "EXEC sp_who"
-$results = Invoke-Sqlcmd -Query $MyQuery -ServerInstance .\SQLExpress -Database JAMS | Format-table | Out-String
-Send-MailMessage -To DanielS@mvpsi.com -From DanielS@mvpsi.com -SmtpServer Berry -Body $results -Subject "My SQL query"
