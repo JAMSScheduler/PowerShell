c2ea40399258fe6e624ca1daf82432605cef0bf6
Dan Fowle-HelpSystems <86322214+DanFowle-HS@users.noreply.github.com>
2022-06-03 10:08:05 -0400

    Add files via upload
    adding Modify maintenance window resource script

---
+++ ManageMaintenanceWindow.ps1
@@ -0,0 +1,12 @@
+﻿
+$resource = Get-Item JAMS::localhost\Resources\"MaintenanceWindow"
+$resource.QuantityAvailable = 0
+$resource.Update()
+
+
+Start-sleep 600
+
+$resource = Get-Item JAMS::localhost\Resources\"MaintenanceWindow"
+$resource.QuantityAvailable = 999999
+$resource.Update()
+
