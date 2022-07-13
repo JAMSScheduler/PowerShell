39bf4b071b68c801cff03ba9ea9dcb8b17e19115
Dan Fowle-HelpSystems <86322214+DanFowle-HS@users.noreply.github.com>
2022-07-13 13:58:25 -0400

    Add files via upload
    Added Create job with job dependency script example.

---
+++ CreateJobWithJobDependency.ps1
@@ -0,0 +1,15 @@
+﻿Import-Module JAMS
+
+##
+# Adjust below path to create job in desired folder (Samples) on desired Scheduler (Localhost)
+##
+$job = new-item -Path "JAMS::Localhost\Samples" -Name "NewSampleCreation" -ItemType "Job" -MethodName "PowerShell"
+$job.Description = "Created from PowerShell" 
+$job.Source = @'
+Start-Sleep 10
+'@
+
+$elem = new-object -Typename MVPSI.JAMS.JobDependency
+$elem.DependOnJobName = "\Samples\SleepJob"
+$job.elements.add($elem)
+$job.Update()
