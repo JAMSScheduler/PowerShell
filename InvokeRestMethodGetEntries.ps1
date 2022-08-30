31d18b08d4befc4488ac9cee52f851199cd77f43
Brey Jackson <jackson.brey@yahoo.com>
2022-08-30 12:35:25 -0400

    Add files via upload

---
+++ InvokeRestMethodGetEntries.ps1
@@ -0,0 +1,42 @@
+﻿#
+# This script makes a REST API call to JAMS and gets all the entries in the monitor, it then cancels/deletes particular entries by name
+#
+
+#The target server to be used in the URI e.g. 'google' in http://www.google.com/
+$restServer = ""
+
+#login information for authentication against the rest server, in this case we're targeting JAMS, so this is a user which can authenticate with the JAMS REST API
+$loginInfo = @{
+    Username = ""
+    Password = ""
+    }
+
+#URI for generating an authentication token
+$AuthURI = ("http://" + $restServer + "/JAMS/api/authentication/login")
+
+#issuing a rest call to JAMS attempting to authenticate with our login info. This should return a token to be used for further authentication
+$authResult = Invoke-RestMethod $AuthURI -Method POST -Body $loginInfo -Verbose
+
+#this builds the authorization header to be used in our various REST actions 
+$headers = @{
+    Authorization = "Bearer " + $authResult.access_token}
+
+#Building the target URI we're going to invoke
+$EntryURI = ("http://" + $restServer + "/jams/api/entry")
+
+#Invoke the method and store the resulting JSON in a PowerShell object
+$getResults = Invoke-RestMethod -Uri $EntryURI -Method GET -Headers $headers -ContentType "application/json" -Verbose
+
+# do some processing on our results
+# Add a list of entries in the results table and output the entries
+$resultTable = @()
+foreach($job in $getResults){
+    $out = New-Object psobject
+    $out | Add-Member noteproperty "Entry" $($job.jamsEntry)
+    $out | Add-Member noteproperty "JobName" $($job.jobName)
+    $out | Add-Member noteproperty "ScheduledTime" $(([dateTime]$job.scheduledTimeUTC).ToLocalTime())
+    $out | Add-Member noteproperty "CurrentStatus" $($job.currentState)
+    $out | Add-Member noteproperty "FinalSeverity" $($job.finalSeverity)
+    $resultTable += $out
+}
+$resultTable | Sort-Object -Property "ScheduledTime" | Format-Table -AutoSize | Out-Host
---
+++ RestSubmit.ps1
@@ -0,0 +1,34 @@
+﻿#
+# This script makes a REST API call to JAMS which will update the value of a property in the submitInfo, then submit the changed job.
+#
+
+#The target server to be used in the URI e.g. 'google' in http://www.google.com/
+$restServer = ""
+
+#login information for authentication against the rest server, in this case we're targeting JAMS, so this is a user which can authenticate with the JAMS REST API
+$loginInfo = @{
+    Username = ""
+    Password = ""}
+
+#issuing a rest call to JAMS attempting to authenticate with our login info. This should return a token to be used for further authentication
+$authResult = Invoke-RestMethod "http://$restServer/JAMS/api/authentication/login" -Method POST -Body $loginInfo
+
+#this builds the authorization header to be used in our various REST actions 
+$headers = @{
+    Authorization = "Bearer " + $authResult.access_token}
+
+#Qualified path of the job source we're trying to submit
+$qualifiedJobPath = "\CustomJobs\RESTAPIJob"
+
+#Building the target URI we're going to invoke for getting the submit info
+$submitInfoURI = ("http://" + $restServer + "/jams/api/submit?name="+$qualifiedJobPath)
+
+#Invoke the method and store the resulting JSON in a PowerShell object
+$submitInfo = Invoke-RestMethod -Uri $submitInfoURI -Method GET -Headers $headers -ContentType "application/json" -Verbose
+
+#Building the target URI we're going to invoke to submit the entry
+$submitURI = ("http://" + $restServer + "/jams/api/submit/")
+
+#Submit the updated submitInfo
+
+$submitResults = Invoke-RestMethod -Uri $submitURI -Method POST -Headers $headers -Body (ConvertTo-JSON $submitinfo -Depth 10) -ContentType "application/JSON" -Verbose
