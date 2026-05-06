a64ac3fa926119b0aaa4e6e35eee75ef9efe507f
Dan Fowle-HelpSystems <86322214+DanFowle-HS@users.noreply.github.com>
2026-05-06 14:31:13 -0400

    update variable acl script
    Moving new script to cookbook, updating existing script for additional clarity.

--- AddMultipleUsersToAllVariablesACL.ps1
+++ AddMultipleUsersToAllVariablesACL.ps1
@@ -1,13 +1,15 @@
-﻿#This script will be used to add ACEs to all Variables in the Dev and Test folders
+﻿#This script will be used to add ACEs to all Variables in the specified folders
 
 #Import the JAMS Modules and mount the JAMS filesystem
 Import-Module JAMS
 New-PSDrive JD JAMS localhost -EA SilentlyContinue
 
-#Specify the AD user(s)/group(s) using domain\user format
-$Identifiers = @("", "")
+#Specify the AD user(s)/group(s) using domain\user format.
+# Can be used for a single identifier, or expanded for a longer list
+$Identifiers = @("ACMECORP\User1", "ACMECORP\Group1")
 
 #Get all variables on a specific folder
+# Modify the folder path to target other folders, or add -Recurse to find all JAMS variables.
 $Variables = Get-ChildItem JD:\ -ObjectType variable 
 
 <#
